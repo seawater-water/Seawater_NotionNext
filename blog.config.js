d71eb64353111899439063052c72851f55529028
Erengo <83535512+seawater-water@users.noreply.github.com>
2024-01-27 08:41:13 +0800

    Update blog.config.js
    简介

--- blog.config.js
+++ blog.config.js
@@ -18,8 +18,8 @@ const BLOG = {
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Erengo', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '生命之树', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://seawater.vercel.app/', // 网站地址
+  BIO: process.env.NEXT_PUBLIC_BIO || '独自走在烟花下', // 作者简介
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://seawaters.top/', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Seawater, 博客', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
